3620143ee001be116190eb41e1dd65bf7ceb8982
WAQAR AHMAD <waqar@revivingcare.com>
2022-09-21 22:40:27 +0500

    all code push to main branch

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+})(jQuery);
+
